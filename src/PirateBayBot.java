a77cca07b0c8f3ac52305f1269053fbf1f72f0e0
unknown <juas83@gmail.com>
2013-11-04 16:53:10 +0100

    sin avances

--- src/PirateBayBot.java
+++ src/PirateBayBot.java
@@ -6,61 +6,59 @@ import java.util.regex.Pattern;
 
 
 public class PirateBayBot {
+	public static void searchTorrent(String search) throws Exception{
+		String urlBase = "thepiratebay.sx";
+        String query = "/search/" + search + "/0/99/0";
         
-        public static void searchTorrent(String search) throws Exception{
-                String urlBase = "thepiratebay.sx";
-                String query = "/search/" + search + "/0/99/0";
-                
-                URI uri = new URI("http", urlBase, query, null);
-                URL url = uri.toURL();
-                
-                String response = ConnectionManager.responseByGetRequest(url, true);
-                
-                listResults(response);
-        }
+        URI uri = new URI("http", urlBase, query, null);
+        URL url = uri.toURL();
         
-        public static void getTorrent(){
-                
-        }
+        String response = ConnectionManager.responseByGetRequest(url, true);
         
-        private static String[] listResults(String httpResponse){
-                String tableRowsRegex = ".*?<table id=\"searchResult\".*?<thead.*?</thead>(.*?)</table>.*?";
-                Pattern p = Pattern.compile(tableRowsRegex);
-                Matcher m = p.matcher(httpResponse);
-            String resultados;
-            if(m.find()) {
+        listResults(response);
+	}
+        
+    public static void getTorrent(){
+            
+    }
+    
+    private static String[] listResults(String httpResponse){
+    	String tableRowsRegex = ".*?\\<table id=\"searchResult\".*?\\<thead.*?\\</thead\\>(.*?)\\</table\\>.*?";
+        Pattern p = Pattern.compile(tableRowsRegex);
+        Matcher m = p.matcher(httpResponse);
+        String resultados;
+        if(m.find()) {
             resultados = m.group(1);
-            String rowRegex = "<tr>(.*?)</tr>";
+            System.out.println(m.group(1));
+            String rowRegex = "\\<tr\\>(.*?)\\</tr\\>";
             p = Pattern.compile(rowRegex);
             m = p.matcher(resultados);
             int count = 0;
             while(m.find()){
-                    count++;
-                    System.out.println("------------ " + count + " ------------");
-                    //String fieldRegex = "<td.*?>(.*?)</td>";
-                    String fieldRegex = "<td.*?>.*?<div.*?class=\"detName\".*?<a\\s*?href=\"(.*?)\".*?class=\"detLink\".*?>(.*?)</a>.*?</td>";
-                    Pattern subP = Pattern.compile(fieldRegex);
-                    //System.out.println(m.group());
-                    Matcher subM = subP.matcher(m.group());
-                    int count2 = 0;
-                    while (subM.find()) {
-                            count2++;
-                            System.out.println(subM.groupCount() + " grupos");
-                            for (int i = 0; i < subM.groupCount(); i++) {
-                                    if(i == 0){
-                                            System.out.print("Enlace: ");
-                                    }else{
-                                            System.out.print("Titulo: ");
-                                    }
-                                    System.out.println(subM.group(i));
-                                        }
-                                }
-                    if(count2 == 0)
-                            System.out.println("NOTHING FOUND");
+            	count++;
+                System.out.println("------------ " + count + " ------------");
+                //String fieldRegex = "<td.*?>(.*?)</td>";
+                String fieldRegex = "\\<td\\sclass=\"vertTh\"\\>\\<a.*?\\>(.*?)\\</a\\>";//.*?<div.*?class=\"detName\".*?<a href=\"(.*?)\".*?class=\"detLink\".*?>(.*?)</a>.*?</td>";
+                Pattern subP = Pattern.compile(fieldRegex);
+                //System.out.println(m.group());
+                Matcher subM = subP.matcher(m.group());
+                int count2 = 0;
+                while (subM.find()) {
+                    count2++;
+                    System.out.println(subM.groupCount() + " grupos");
+                    for (int i = 1; i < subM.groupCount(); i++) {
+                        if(i == 1){
+                        	System.out.print("Categoría: ");
+                        }else{
+                            System.out.print("Titulo: ");
+                        }
+                        System.out.println(subM.group(i));
+                    }
+                }
+                if(count2 == 0)
+        			System.out.println("NOTHING FOUND");
             }
-            }else{
-                    System.out.println("NOTHING FOUND!!!");
-            }
-                return new String[5];
         }
+        return new String[5];
+    }
 }
